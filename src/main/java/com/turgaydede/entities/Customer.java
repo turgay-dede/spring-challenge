62914dbea1b97161a2bc5529dc792c570b4f4308
turgay-dede <turgay.dede28@hotmail.com>
2022-02-28 17:02:31 +0300

    customer entity created

---
+++ src/main/java/com/turgaydede/entities/Customer.java
@@ -0,0 +1,32 @@
+package com.turgaydede.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "company_id")
+    private int companyId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+}
